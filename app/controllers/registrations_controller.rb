71fad893ab17aec05a5f566e9578b07dbee9e5c9
tdpaul315@gmail.com <tiaunaj.tsg@gmail.com>
2020-11-11 18:52:42 -0500

    adds columns to users table and omniauth for github

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,19 @@
+class RegistrationsController < Devise::RegistrationsController
+
+    
+    
+    
+    private
+    
+    def sign_up_params
+    params.require(:user).permit(:name, :username, :email, :password, :password_confirmation, :admin)
+    end
+    
+    def account_update_params
+    params.require(:user).permit(:name, :username, :email, :password, :password_confirmation, :current_password, :admin)
+    end
+    
+    
+    
+
+end 
